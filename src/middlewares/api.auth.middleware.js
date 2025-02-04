77b0cc750c7e4dfd9e69c49b961c383801e631b5
MH TOUFIK <toufikforyou@gmail.com>
2025-02-04 21:17:40 +0600

    update api authentication middleware to include default api key

--- src/middlewares/api.auth.middleware.js
+++ src/middlewares/api.auth.middleware.js
@@ -1,11 +1,12 @@
 import ApiResponse from "../models/api.response.model.js";
 
 export default (req, res, next) => {
-  const apiKey = req.headers["x-api-key"] || req.query.api_key;
+  const apiKey =
+    req.headers["x-api-key"] || req.query.api_key || "your-api-key";
   const serverIp = req.headers["cf-connecting-ip"] || req.ip;
   const hostname = req.hostname;
 
-  console.log(serverIp, hostname, process.env.API_KEY);
+  console.log(serverIp, hostname);
 
   if (apiKey !== process.env.API_KEY) {
     return res.status(401).json(
