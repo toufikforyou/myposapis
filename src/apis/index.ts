92c9cd074adaea3954cc2743eae8a2c6ece243ad
MH TOUFIK <toufikforyou@gmail.com>
2025-02-08 22:54:57 +0600

    Refactor API routes and improve error handling

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,13 +1,9 @@
-import { Response, Router } from "express";
+import { Router } from "express";
 import apiAuthMiddleware from "../middlewares/api.auth.middleware";
 import v1Routes from "./v1";
 
 const apiRouter: Router = Router();
 
 apiRouter.use("/v1", apiAuthMiddleware, v1Routes);
-
-apiRouter.use((_, res: Response) => {
-  res.redirect("/404");
-});
 
 export default apiRouter; 
--- src/apis/v1/index.ts
+++ src/apis/v1/index.ts
@@ -1,15 +1,10 @@
-import { Response, Router } from "express";
+import { Router } from "express";
 import ApiResponse from "../../models/api.response.model";
+import v1Routes from "./routes";
 
 const router: Router = Router();
 
-router.get("/", (_, res: Response) => {
-  res.json(
-    new ApiResponse.Success(200, "This is a version 1", {
-      page: "Api version 1 home page",
-    })
-  );
-});
+router.use("/", v1Routes);
 
 router.use(() => {
   throw new ApiResponse.Error(404, "API version 1 route not found!");
